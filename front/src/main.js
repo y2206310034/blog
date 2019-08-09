fe8d29b5d1f319d300dad8aaefef7ee0336e0d50
yang <2206310034@qq.com>
2019-08-09 16:56:36 +0800

    对前台页面的elementui进行了按需引入,将图片换成ico格式的

--- front/src/main.js
+++ front/src/main.js
@@ -3,14 +3,43 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router' 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import VueLazyload from 'vue-lazyload'
-import store from "./store"
-
 // 引入代码高亮
 import hljs from 'highlight.js';
 import 'highlight.js/styles/vs2015.css' //样式文件
+import {
+        Pagination,
+        Row,
+        Col,
+        Icon,
+        Container,
+        Header,
+        Aside,
+        Main,
+        Footer,
+        Button,
+        Loading} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+
+import VueLazyload from 'vue-lazyload'
+import store from "./store"
+
+
+Vue.use(Pagination);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Icon);
+Vue.use(Container);
+Vue.use(Header);
+Vue.use(Aside);
+Vue.use(Main);
+Vue.use(Footer);
+Vue.use(Button);
+Vue.use(Loading);
+
+
+
+// 代码高亮自定义指令
 Vue.directive('highlight',function (el) {
   let blocks = el.querySelectorAll('pre code');
   setTimeout(() =>{
@@ -20,13 +49,13 @@ Vue.directive('highlight',function (el) {
   }, 200)
 })
 
-
-
-Vue.use(ElementUI)
+// Vue.use(ElementUI);
+// 图片懒加载
 Vue.use(VueLazyload,{
   preLoad: 0.9,
   attempt: 1
 })
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
